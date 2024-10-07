a4b0f4ba0bf3579ab1c39e185d5562ccb388a43f
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-10-07 08:31:52 -0500

    Test with IPC

--- index.js
+++ index.js
@@ -7,18 +7,12 @@ const client = new RPC.Client({ transport: "ipc" });
 
 client.on("ready", async () => {
   console.log("Logged in as", client.application.name);
-
-  // mute your self
-  await client.setVoiceSettings({
-    deaf: true
-  })
-
-  console.log("Voice settings updated");
 });
 
 // Log in to RPC with client id
 client.login({ 
   clientId: CLIENT_ID, 
+  prompt: "none",
   clientSecret: CLIENT_SECRET,
   scopes, 
   redirectUri: "http://localhost:3000"
